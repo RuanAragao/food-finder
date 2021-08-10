eb111063cabbd055a13adfe28ced17a83e3007b8
Ruan Aragão <ruanaragao2@gmail.com>
2021-08-09 22:38:29 -0300

    Inclui skeleton nos cards de busca

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 
 import ReactStars from 'react-rating-stars-component';
 
 import photo_default from '../../assets/restaurante-fake.png';
+import Skeleton from '../Skeleton';
 import {
   Restaurant,
   RestaurantInfo,
@@ -11,6 +13,8 @@ import {
 } from './style';
 
 const RestaurantCard = ({ restaurant, onClick }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <Restaurant onClick={onClick}>
       <RestaurantInfo>
@@ -25,7 +29,13 @@ const RestaurantCard = ({ restaurant, onClick }) => {
         />
         <RestaurantAddress>{restaurant.vicinity || restaurant.formatted_address}</RestaurantAddress>
       </RestaurantInfo>
-      <RestaurantThumb src={restaurant.photos ? restaurant.photos[0].getUrl() : photo_default} alt={restaurant.name} />
+      <RestaurantThumb
+        imageLoaded={imageLoaded}
+        src={restaurant.photos ? restaurant.photos[0].getUrl() : photo_default}
+        onLoad={() => setImageLoaded(true)}
+        alt={restaurant.name}
+      />
+      {!imageLoaded && <Skeleton width="100px" height="100px" />}
     </Restaurant>
   )
 };
