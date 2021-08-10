1f691553923cdca57c09eb52bdc56f1b8eeba844
Ruan Aragão <ruanaragao2@gmail.com>
2021-08-09 22:24:55 -0300

    Criar componente skeleton

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -24,6 +24,7 @@ export const MapContainer = (props) => {
 
   function getRestaurantById(placeId) {
     const service = new google.maps.places.PlacesService(map);
+    dispatch(setRestaurant(null));
 
     const request = {
       placeId,
@@ -45,6 +46,7 @@ export const MapContainer = (props) => {
 
   function searchByQuery(query) {
     const service = new google.maps.places.PlacesService(map);
+    dispatch(setRestaurants([]));
 
     const request = {
       location: map.center,
@@ -62,6 +64,7 @@ export const MapContainer = (props) => {
 
   function searchNearby(map, center) {
     const service = new google.maps.places.PlacesService(map);
+    dispatch(setRestaurants([]));
 
     const request = {
       location: center,
