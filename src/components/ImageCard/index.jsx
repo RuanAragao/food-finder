5ecd72b6d1656203be07352e6392a215cc47343f
Ruan Aragão <ruanaragao2@gmail.com>
2021-08-09 23:41:42 -0300

    Adiciona skeleton no carousel

--- src/components/ImageCard/index.jsx
+++ src/components/ImageCard/index.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+
+import Skeleton from '../Skeleton';
 
 const Card = styled.div`
   display: flex;
@@ -18,10 +21,24 @@ const Title = styled.span`
 `;
 
 const ImageCard = ({ photo, title }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const imageLoader = new Image();
+    imageLoader.src = photo;
+    imageLoader.onload = () => setImageLoaded(true);
+  }, [photo]);
+
   return (
-    <Card photo={photo}>
-      <Title>{title}</Title>
-    </Card>
+    <>
+      {imageLoaded ? (
+        <Card photo={photo}>
+          <Title>{title}</Title>
+        </Card>
+      ) : (
+        <Skeleton width="90px" height="90px" />
+      )}
+    </>
   )
 };
 
